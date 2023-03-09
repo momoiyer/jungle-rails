05f4e782ce2234dfd52ed0a0e3b334ac084e8e14
kmomozin <khinmomozin@gmail.com>
2023-03-09 12:23:42 -0700

    add initial user registration form

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,29 @@
 class UserController < ApplicationController
+  def index
+    @user = User.new
+  end
+
+  def create
+    p "here to create #{@user}"
+    p "here to create #{user_params}"
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path, notice: 'User registered!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    
+    params.require(:"/user").permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password_digest
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,11 @@ Rails.application.routes.draw do
   
   resources :about, only: [:index]
 
+  resources :user, only: [:index, :create] 
+  # do
+  #   post :create
+  # end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
